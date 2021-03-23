c233587cd90a6b72b6826514620eb1108c6bce4f
SANGDONKIM <kksd904@gmail.com>
2021-03-23 21:19:51 +0900

    update

--- R/walmart.R
+++ R/walmart.R
@@ -55,20 +55,178 @@ train <- train %>% select(-starts_with('mark'))
 test <- test %>% select(-starts_with('mark'))
 
 
+names(train)
+
+
 # preprocessing 
 
 walmart_recipe <- 
     recipe(weekly_sales ~ .,
            data = train) %>%
-    step_date(date, features = c('month')) %>% 
+    step_date(date, features = c('month', 'year')) %>% 
     step_rm(date) %>%
     step_mutate(store = as.factor(store), 
                 dept = as.factor(dept)) %>%
     step_dummy(all_nominal()) %>% 
-    step_normalize(all_numeric(), -all_outcomes())
+    step_zv(all_predictors()) %>% 
+    step_nzv(all_predictors()) %>% 
+    step_normalize(all_numeric(), -all_outcomes()) %>% 
+    prep()
 
 
-walmart_recipe %>% juice()
+baked_train <- walmart_recipe %>% 
+    bake(train)
+
+baked_test <- walmart_recipe %>% 
+    bake(test)
+
+
+set.seed(1234)
+
+cv_data <- vfold_cv(baked_train, v = 5, strata = weekly_sales)
+
+
+
+# random forest 
+
+tune_spec <- rand_forest(
+    mtry = tune(),
+    trees = 1000,
+    min_n = tune()
+) %>%
+    set_mode("regression") %>%
+    set_engine("ranger")
+
+
+params <- parameters(tune_spec) %>%
+    finalize(baked_train)
+
+rf_wflow <- workflow() %>%
+    add_recipe(walmart_recipe) %>%
+    add_model(tune_spec)
+
+
+options(tidymodels.dark = TRUE)
+cl <- makePSOCKcluster(6)
+registerDoParallel(cl)
+
+
+folds <- vfold_cv(baked_train, v = 5)
+
+tuned <- tune_bayes(
+    object = rf_wflow,
+    resamples = folds,
+    param_info = params,
+    iter = 30,
+    metrics =  metric_set(rmse, mape),
+    initial = 10,
+    control = control_bayes(
+        verbose = TRUE,
+        no_improve = 10,
+        seed = 123
+    )
+)
+
+show_best(tuned, 'rmse') %>% 
+    select(1:7, 11) %>% 
+    gt()
+
+
+best_model <- select_best(tuned, "rmse")
+
+final_model <- finalize_model(mod, best_model)
+walmart_workflow <- xgboost_wflow %>% update_model(final_model)
+xgb_fit <- fit(walmart_workflow, data = train)
+
+
+pred <- 
+    predict(xgb_fit, test) %>% 
+    mutate(modelo = "XGBoost")
+
+pred
+
+
+
+
+# xgboost 
+
+# grid search 
+
+xgboost_model<-
+    parsnip::boost_tree(
+        mode = "regression",
+        trees = 1000,
+        min_n = tune(),
+        tree_depth = tune(),
+        ) %>%
+    set_engine('xgboost', objective = "reg:squarederror",verbose=-1)
+
+
+xgboost_params <-
+    dials::parameters(
+        min_n(),
+        tree_depth()
+    )
+
+
+xgboost_grid <- dials::grid_max_entropy(
+    xgboost_params,
+    size = 30
+)
+
+
+xgboost_wf <- workflow() %>%
+    add_model(xgboost_model) %>%
+    add_formula(weekly_sales ~ .)
+
+
+xgboost_tuned <- tune::tune_grid(
+    object = xgboost_wf,
+    resamples = cv_data,
+    grid = xgboost_grid,
+    metrics = yardstick::metric_set(rmse),
+    control = tune::control_grid(verbose = FALSE)
+)
+
+xgboost_tuned %>%
+    tune::show_best(metric = "rmse",n = 5)
+
+xgboost_best_params <- xgboost_tuned %>%
+    tune::select_best("rmse")
+
+# Best parameters (tuned)
+xgboost_model_final <- xgboost_model%>%
+    tune::finalize_model(xgboost_best_params)
+
+xgboost_model_final
+
+train_prediction <- xgboost_model_final %>%
+    parsnip::fit(weekly_sales ~., data = train) %>% 
+    stats::predict(new_data = train) %>%
+    dplyr::bind_cols(train)
+
+
+
+test_prediction <- xgboost_model_final %>%
+    parsnip::fit(weekly_sales ~., data = train) %>% 
+    stats::predict(new_data = test) %>%
+    dplyr::bind_cols(test)
+
+
+test_prediction_submission <- xgboost_model_final %>%
+    parsnip::fit(weekly_sales ~., data = train) %>% 
+    stats::predict(new_data = test)
+
+test_prediction_submission$.pred
+
+subfile <- read_csv("./data/walmart/sampleSubmission.csv.zip")
+subfile
+
+subfile$Weekly_Sales <- test_prediction_submission$.pred
+
+write.csv(subfile, row.names = FALSE,
+          "./data/walmart/xgboost_base_gridsearch.csv")
+
 
 
 # https://www.hfshr.xyz/posts/2020-05-23-tidymodel-notes/
@@ -131,6 +289,10 @@ pred <-
     mutate(modelo = "XGBoost")
 
 pred
+
+
+
+
 
 subfile <- read_csv("./data/walmart/sampleSubmission.csv.zip")
 subfile
