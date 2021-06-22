c06c20d9577cce6c5e60e9ea7d83269f2d9a9ebb
Your Name <shj970314@naver.com>
2021-06-22 18:59:56 +0900

    수정, 2021.csv 하루 누락 수정 및 변수추가 후 모델링

--- study-presentation/seongho-June-19/날씨데이터 다루기.R
+++ study-presentation/seongho-June-19/날씨데이터 다루기.R
@@ -30,16 +30,115 @@ im<-im %>% filter(pcnt>94) %>% select(col_name) %>% as_vector()
 
 weather<-weather %>% select(-im)
 inspect_na(weather) %>% show_plot()
-# 시간데이터로 한다면, 오전시간대 평균 (ex. 09~12),오후시간대 평균 (ex.12~18)의 평균값으로 각각 변수들 추가
+# 시간데이터로 한다면, 오전시간대 대표값12시,오후시간대 대표값18시) 각각 변수들 추가
+colnames(weather)
 weather %>% 
-  mutate(year=as.numeric(substr(weather$일시,1,4)),
-                   month=as.numeric(substr(weather$일시,6,7)),
-                   day=as.numeric(substr(weather$일시,9,10)),
+  mutate(연=as.numeric(substr(weather$일시,1,4)),
+                   월=as.numeric(substr(weather$일시,6,7)),
+                   일=as.numeric(substr(weather$일시,9,10)),
                    hour=as.numeric(substr(weather$일시,12,13))) %>%
-  filter((hour>=9 & hour<=12) | (hour>=15 & hour<=18)) %>% 
-  mutate(lunchdinner= factor(ifelse(hour<=12,"오전","오후"))) %>% 
-  group_by(lunchdinner,year,month,day)  
-  
-           
-  
+  filter((hour==12) | (hour==18)) %>% 
+  mutate(lunchdinner= factor(ifelse(hour==12,"오전","오후"))) -> weather_select
+
+colnames(weather_select)<-c("지점" , "일시"   , "기온" ,"강수량"      ,"풍속"  , "풍향"      ,
+                            "습도"  ,  "증기압" ,  "이슬점온도", "현지기압"  , "해면기압" ,    
+                            "일조" ,  "일사" , "전운량",  "중하층운량", "최저운고"   ,
+                            "시정",  "지면온도","연","월","일", "hour" , "lunchdinner"  )
+weather_select[is.na(weather_select$강수량),]$강수량<-0
+weather_select$지면온도<-abs(weather_select$지면온도)
+weather_select$기온<-abs(weather_select$기온)
+weather_select %>% 
+  recipe(지점~.) %>% 
+  step_impute_knn(all_numeric()) %>% 
+  prep() %>% juice() ->weather_select
+
+
+
+train_base_lunch %>% select(연,월,일,요일)->imsi
+test_base_dinner %>% select(연,월,일,요일)->imsi2
+
+left_join(imsi2,weather_select,c("연","월","일"))->weather_select_test
+left_join(imsi,weather_select,c("연","월","일"))->weather_select
+
+
+
+
+
+weather_select %>% select(-is.integer) %>% filter(lunchdinner=="오전")->morning
+weather_select %>% select(-is.integer) %>% filter(lunchdinner=="오후")->afternoon
+
+weather_select_test %>% select(-is.integer) %>% filter(lunchdinner=="오전")->morning_test
+weather_select_test %>% select(-is.integer) %>% filter(lunchdinner=="오후")->afternoon_test
+
+
+
+morning %>% select(-c(요일,일시,hour,lunchdinner))->morning
+afternoon %>% select(-c(요일,일시,hour,lunchdinner))->afternoon
+
+train_weather_lunch<-left_join(train_base_lunch,morning, c("연","월","일"))
+train_weather_dinner<-left_join(train_base_dinner,afternoon, c("연","월","일"))
+
+morning_test %>% select(-c(요일,일시,hour,lunchdinner))->morning_test
+afternoon_test %>% select(-c(요일,일시,hour,lunchdinner))->afternoon_test
+
+ 
+test_weather_lunch<-left_join(test_base_lunch,morning_test, c("연","월","일"))
+test_weather_dinner<-left_join(test_base_dinner,afternoon_test, c("연","월","일"))
+train_weather_lunch %>% inspect_cor() %>%  filter(col_1 %in%c("석식계","중식계") |
+                                               col_2 %in%c("석식계","중식계")) %>% show_plot()
+train_weather_dinner%>% inspect_cor() %>%  filter(col_1 %in%c("석식계","중식계") |
+                                                    col_2 %in%c("석식계","중식계")) %>% show_plot()
+
+
+
+
+
+
+# 모델링 ---------------------------------------------------------------------
+
+
+#### XGBoost - basic
+
+cores <- parallel::detectCores()
+cores
+
+set.seed(125)
+xgb_mod <- boost_tree() %>% 
+  set_mode("regression") %>% 
+  set_engine("xgboost",
+             num.threads = cores)
+
+
+xgb_wf_lunch <- 
+  workflow() %>% 
+  add_model(xgb_mod) %>% 
+  add_formula(중식계~.)
+xgb_fit_lunch <- xgb_wf_lunch %>% 
+  fit(train_weather_lunch)
+xgb_pred_lunch <- predict(xgb_fit_lunch,new_data = test_weather_lunch)
+
+xgb_wf_dinner <- 
+  workflow() %>% 
+  add_model(xgb_mod) %>% 
+  add_formula(석식계~.)
+xgb_fit_dinner <- xgb_wf_dinner %>% 
+  fit(train_weather_dinner)
+xgb_pred_dinner <- predict(xgb_fit_dinner,new_data = test_weather_dinner)
+
+#변수중요도
+xgb_fit_lunch%>% 
+  pull_workflow_fit() %>% 
+  vip(num_features = 30)
+xgb_fit_dinner %>% 
+  pull_workflow_fit() %>% 
+  vip(num_features = 30)
+
+xb_pred_lunch <- predict(xgb_fit_lunch, new_data = test_weather_lunch) 
+xb_pred_dinner <- predict(xgb_fit_dinner, new_data = test_weather_dinner) 
+
+sample_submission$중식계<-xb_pred_lunch$.pred
+sample_submission$석식계<-xb_pred_dinner$.pred
+
+write.csv(sample_submission,"xgboost_model+weather.csv",row.names = F,fileEncoding = "UTF-8")
+
 
